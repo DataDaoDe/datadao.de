b4204e0eaf9192ac76a265e341997dc23208c103
John Faucett <john.faucett@protonmail.com>
2019-09-28 20:16:56 +0200

    [build 001]

---
+++ build/js/katex-setup.js
@@ -0,0 +1,9 @@
+$(document).ready(function () { // this need jquery
+  renderMathInElement(document.body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ]
+  });
+});
